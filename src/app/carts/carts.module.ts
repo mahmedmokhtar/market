a28196e4e8eee4e5171cd08003f2df009c0dae9d
mahmedmokhtar <https://github.com/mahmedmokhtar>
2023-06-21 20:25:01 +0300

    update cart

--- src/app/carts/carts.module.ts
+++ src/app/carts/carts.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './components/cart/cart.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -11,7 +12,10 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ]
 })
-export class CartsModule { }
+export class CartsModule {
+
+}
--- src/app/carts/components/cart/cart.component.ts
+++ src/app/carts/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -8,11 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class CartComponent implements OnInit {
   datepro:any[] = [];
   total = 0;
-constructor(){
+  fintotal:any;
+constructor(private cartser:CartService){
 
 }
 ngOnInit() {
 this.getcartonlocal()
+this.getalltotal()
 }
 getcartonlocal(){
   if("data" in localStorage){
@@ -20,11 +23,39 @@ getcartonlocal(){
   }
   this.getalltotal()
 }
+
+deletecart(i:any){
+  this.datepro.splice(i,1)
+  this.getalltotal();
+  localStorage.setItem("data" , JSON.stringify(this.datepro))
+}
+addtotol(i:any){
+this.datepro[i].quantity++
+this.getalltotal()
+localStorage.setItem("data" , JSON.stringify(this.datepro))
+
+}
+mintotol(i:any){
+  this.datepro[i].quantity--;
+  this.getalltotal()
+  localStorage.setItem("data" , JSON.stringify(this.datepro))
+
+}
+resualt(){
+  localStorage.setItem("data" , JSON.stringify(this.datepro))
+
+}
+cleardate(){
+
+  this.datepro = []
+  localStorage.setItem("data" , JSON.stringify(this.datepro))
+this.getalltotal()
+}
 getalltotal(){
   this.total = 0;
   for(let x = 0 ; x < this.datepro.length ; x++){
     // this.total = (this.datepro[x].data.price * this.datepro[x].quantity)
-    this.total += this.datepro[x].data.price * this.datepro[x].quantity
+    this.total += Math.ceil(this.datepro[x].data.price * this.datepro[x].quantity)
 
   }
 }
